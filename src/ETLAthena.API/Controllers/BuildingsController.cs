b8c3df2e5e479e9810401bc6d2a53b6bd7b8c8eb
Robert Ruta <robertruta50@gmail.com>
2023-11-25 17:14:50 +0100

    Added an buildings/all-buildings endpoint that serves as a HTTP getter of all building data stored in the data store.

--- src/ETLAthena.API/Controllers/BuildingsController.cs
+++ src/ETLAthena.API/Controllers/BuildingsController.cs
@@ -1,13 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using ETLAthena.Core.Models;
+using ETLAthena.Core.Services;
+using ETLAthena.Core.DataStorage;
 
 namespace ETLAthena.API.Controllers
 {
+    
     [ApiController]
     [Route("[controller]")]
     public class BuildingsController : ControllerBase
     {
-        [HttpGet]
+        private readonly IDataStorageService _dataStorageService;
+
+        public BuildingsController(IDataStorageService dataStorageService)
+        {
+            _dataStorageService = dataStorageService;
+        }
+
+
+        [HttpGet("test-buildings")]
         public ActionResult<IEnumerable<BuildingModel>> GetBuildings()
         {
             var buildings = new List<BuildingModel>
@@ -16,6 +27,14 @@ namespace ETLAthena.API.Controllers
                 new BuildingModel { Id = 2, Name = "Building 2", Address = "456 Elm St" }
             };
 
+            return Ok(buildings);
+        }
+        
+        
+        [HttpGet("all-buildings")]
+        public ActionResult<IEnumerable<BuildingModel>> GetAllBuildings()
+        {
+            var buildings = _dataStorageService.GetAllBuildings();
             return Ok(buildings);
         }
     }
