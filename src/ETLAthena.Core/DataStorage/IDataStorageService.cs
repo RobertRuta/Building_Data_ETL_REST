69446209e8e5c6cbcfa2421fb3d335878f443c9d
Robert Ruta <robertruta50@gmail.com>
2023-11-25 11:02:59 +0100

    Implemented in-memory data store along with the interface for data stores.

--- src/ETLAthena.Core/DataStorage/IDataStorageService.cs
+++ src/ETLAthena.Core/DataStorage/IDataStorageService.cs
@@ -1,4 +1,9 @@
+using ETLAthena.Core.Models;
 namespace ETLAthena.Core.DataStorage
 {
-
+    public interface IDataStorageService
+    {
+        BuildingModel GetBuilding(int id);
+        void UpdateOrCreateBuilding(BuildingModel building);
+    }
 }
--- src/ETLAthena.Core/DataStorage/InMemoryDataStore.cs
+++ src/ETLAthena.Core/DataStorage/InMemoryDataStore.cs
@@ -1,4 +1,29 @@
+using ETLAthena.Core.Models;
+
 namespace ETLAthena.Core.DataStorage
 {
+    public class InMemoryDataStore : IDataStorageService
+    {
+        private readonly Dictionary<int, BuildingModel> _buildings;
 
+        public InMemoryDataStore()
+        {
+            _buildings = new Dictionary<int, BuildingModel>();
+        }
+
+        public BuildingModel GetBuilding(int id)
+        {
+            _buildings.TryGetValue(id, out var building);
+            return building;
+        }
+
+        public void UpdateOrCreateBuilding(BuildingModel building)
+        {
+            if (building == null) return;
+
+            _buildings[building.Id] = building;
+        }
+
+        // Additional methods can be implemented as required.
+    }
 }
