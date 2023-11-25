030256748a626054c80101d8edd833191487386d
Robert Ruta <robertruta50@gmail.com>
2023-11-25 20:18:46 +0100

    Added data ingestion endpoints.

--- src/ETLAthena.API/Controllers/BuildingsController.cs
+++ src/ETLAthena.API/Controllers/BuildingsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using ETLAthena.Core.Models;
 using ETLAthena.Core.Services;
 using ETLAthena.Core.DataStorage;
+using System.Text.Json;
 
 namespace ETLAthena.API.Controllers
 {
@@ -11,31 +12,82 @@ namespace ETLAthena.API.Controllers
     public class BuildingsController : ControllerBase
     {
         private readonly IDataStorageService _dataStorageService;
+        private readonly IDataIngestionService _dataIngestionService;
 
-        public BuildingsController(IDataStorageService dataStorageService)
+        public BuildingsController(IDataStorageService dataStorageService, IDataIngestionService dataIngestionService)
         {
             _dataStorageService = dataStorageService;
+            _dataIngestionService = dataIngestionService;
         }
 
-
-        [HttpGet("test-buildings")]
-        public ActionResult<IEnumerable<BuildingModel>> GetBuildings()
-        {
-            var buildings = new List<BuildingModel>
-            {
-                new BuildingModel { Id = 1, Name = "Building 1", Address = "123 Main St" },
-                new BuildingModel { Id = 2, Name = "Building 2", Address = "456 Elm St" }
-            };
-
-            return Ok(buildings);
-        }
-        
         
         [HttpGet("all-buildings")]
         public ActionResult<IEnumerable<BuildingModel>> GetAllBuildings()
         {
             var buildings = _dataStorageService.GetAllBuildings();
             return Ok(buildings);
+        }
+
+        
+        [HttpPost("ingest/S1/single")]
+        public IActionResult IngestS1Single([FromBody] JsonElement jsonData)
+        {
+            try
+            {
+                _dataIngestionService.IngestDataFromSourceS1(jsonData.ToString()); // Assuming a generic ingestion method
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // Log exception and return appropriate error response
+                return BadRequest(ex.Message);
+            }
+        }
+        
+        [HttpPost("ingest/S1/bulk")]
+        public IActionResult IngestS1Bulk([FromBody] JsonElement jsonDataList)
+        {
+            try
+            {
+                _dataIngestionService.IngestBulkDataFromSourceS1(jsonDataList.ToString()); // Ingest each record
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // Log exception and return appropriate error response
+                return BadRequest(ex.Message);
+            }
+        }
+
+        
+        [HttpPost("ingest/S2/single")]
+        public IActionResult IngestS2Single([FromBody] JsonElement jsonData)
+        {
+            try
+            {
+                _dataIngestionService.IngestDataFromSourceS2(jsonData.ToString()); // Assuming a generic ingestion method
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // Log exception and return appropriate error response
+                return BadRequest(ex.Message);
+            }
+        }
+        
+        [HttpPost("ingest/S2/bulk")]
+        public IActionResult IngestS2Bulk([FromBody] JsonElement jsonDataList)
+        {
+            try
+            {
+                _dataIngestionService.IngestBulkDataFromSourceS2(jsonDataList.ToString()); // Ingest each record
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // Log exception and return appropriate error response
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
