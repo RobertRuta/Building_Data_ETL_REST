8401b5bab72c3838c50691be79bb3d1a7f2415d0
Robert Ruta <robertruta50@gmail.com>
2023-11-25 18:08:07 +0100

    Made some fields nullable in S1 and S2 models.

--- src/ETLAthena.Core/Models/S1Model.cs
+++ src/ETLAthena.Core/Models/S1Model.cs
@@ -3,10 +3,10 @@ namespace ETLAthena.Core.Models
     public class S1Model
     {
         public int Id { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
         public double Lat { get; set; }
         public double Lon { get; set; }
-        public int FloorCount { get; set; }
+        public int? FloorCount { get; set; }
     }
 }
--- src/ETLAthena.Core/Models/S2Model.cs
+++ src/ETLAthena.Core/Models/S2Model.cs
@@ -3,10 +3,10 @@ namespace ETLAthena.Core.Models
     public class S2Model
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Postcode { get; set; }
-        public double[] Coordinates { get; set; }
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+        public string? Postcode { get; set; }
+        public double[]? Coordinates { get; set; }
         public int? FloorArea { get; set; }
     }
 }
