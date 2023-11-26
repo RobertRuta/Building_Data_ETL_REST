b996d0024a4a188dd7e4c710ad52e8ac85425d2d
Robert Ruta <robertruta50@gmail.com>
2023-11-27 00:11:56 +0100

    Improved the safe int converter.

--- src/ETLAthena.Core/Services/Helpers/SafeIntConverter.cs
+++ src/ETLAthena.Core/Services/Helpers/SafeIntConverter.cs
@@ -10,18 +10,27 @@ public class SafeIntConverter : JsonConverter
     public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
     {
         if (reader.TokenType == JsonToken.Null)
-            return objectType == typeof(int) ? -1 : (int?)null;
+        {   
+            return objectType == typeof(int) ? (int)0 : (int?)null;
+        }
 
         if (reader.TokenType == JsonToken.Integer)
-            return reader.Value;
+        {   
+            var val = (long)reader.Value;
+            if (val >= int.MinValue && val <= int.MaxValue)
+                    return (int)val; // Safely cast to int
+                throw new JsonSerializationException($"Integer value {val} is too large for an Int32.");
+        }
 
         if (reader.TokenType == JsonToken.String)
         {
             if (int.TryParse(reader.Value.ToString(), out int parsedInt))
                 return parsedInt;
+            else
+                return (int?)null;
         }
 
-        return objectType == typeof(int) ? -1 : (int?)null;
+        return objectType == typeof(int) ? (int)0 : (int?)null;
     }
 
     public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
